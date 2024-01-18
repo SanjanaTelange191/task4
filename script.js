ce19618ab804ecf39791efb012339d2de7a15ede
SanjanaTelange191 <sanjanatelange499@gmail.com>
2024-01-18 07:09:13 +0530

    fourthtask

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+        Strings: ["Frontend Developer" , "YouTuber","Web Developer"],
+        typeSeed:100,        
+        backSpeed:100,
+        backDelay:1000,
+        loop: true
+}
+);
